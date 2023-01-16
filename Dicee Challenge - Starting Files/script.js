83a7ca592da526ac8b6a792861decc076eade77d
Austin Pereira <79310192+AUSt-in@users.noreply.github.com>
2023-01-16 13:18:08 -0800

    Update script.js

--- Dicee Challenge - Starting Files/script.js
+++ Dicee Challenge - Starting Files/script.js
@@ -1,20 +1,20 @@
-var randomNumber1 = Math.floor(Math.random() * 6)+1; //1-6
-var randomDiceImg = "dice" + randomNumber1 + ".png";
-var randomDiceImgSource = "images/" + randomDiceImg;
-var image1 = document.querySelectorAll(".img")[0];
-image1.setAttribute("src", randomDiceImgSource);
-var randomNumber2 = Math.floor(Math.random() * 6)+1; //1-6
-var randomDiceImg2 = "dice" + randomNumber2 + ".png";
-var randomDiceImgSource2 = "images/" + randomDiceImg2;
-var image2 = document.querySelectorAll(".img")[1];
-image1.setAttribute("src", randomDiceImgSource2);
+// Die 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-if(randomNumber1>randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 wins";
-}
-else if(randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 wins";
-}
-else{
-    document.querySelector("h1").innerHTML = "DRAW";
+var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource1);
+
+// Die 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+// Title
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
 }
